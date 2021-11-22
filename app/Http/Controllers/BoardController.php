82cc3981c9c294b3d49b28e7fe6f9cc99d335f3d
dev9163 <dev9163@linkareer.com>
2021-11-22 10:02:32 +0900

    update README.md

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -11,7 +11,7 @@ class BoardController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except(['index', 'show']);
     }
 
     public function index()
